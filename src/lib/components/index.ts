a3e6d2ac32d09476bb6681bb14b8fccd9146d7b6
Raman Verma <ramanverma4183@gmail.com>
2025-08-29 10:53:02 +0530

    svelteKit app

---
+++ src/lib/components/index.ts
@@ -0,0 +1,1 @@
+export { default as CompositionGrid } from './dev/CompositionGrid.svelte';
---
+++ src/routes/+page.svelte
@@ -0,0 +1,24 @@
+<!-- Javascript -->
+<script lang="ts">
+    import { CompositionGrid } from "$lib/components";
+
+    // COLORS
+    // #3c6e71
+    // #ffffff
+    // #d9d9d9
+    // #284b63
+</script>
+
+<!-- HTML -->
+<CompositionGrid>
+    <main></main>
+</CompositionGrid>
+
+<!-- Styles -->
+<style>
+    :global(body) {
+        margin: 0;
+        background-color: #353535;
+        height: 100vh;
+    }
+</style>
